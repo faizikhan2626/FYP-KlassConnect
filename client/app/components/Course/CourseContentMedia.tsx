a6dfcb3051bb44a6097760584c0874a384de979e
faizankhan2626 <faizikhan2626@gmail.com>
2025-04-20 09:43:57 +0500

    Fixed gitignore for server folder

--- client/app/components/Course/CourseContentMedia.tsx
+++ client/app/components/Course/CourseContentMedia.tsx
@@ -34,12 +34,13 @@ const CourseContentMedia = ({
   user,
   refetch,
 }: Props) => {
-  const [activeBar, setActiveBar] = useState(1);
+  const [activeBar, setActiveBar] = useState(0);
   const [question, setQuestion] = useState("");
   const [rating, setRating] = useState(0);
   const [review, setReview] = useState("");
   const [answer, setAnswer] = useState("");
   const [questionId, setQuestionId] = useState("");
+
   const [
     addAnswerInQuestion,
     {
@@ -76,26 +77,40 @@ const CourseContentMedia = ({
       refetch();
       toast.success("Question added successfully!");
     }
+    if (answerSuccess) {
+      setAnswer("");
+      refetch();
+      toast.success("Reply added successfully!");
+    }
     if (error) {
       if ("data" in error) {
         const errorMessage = error as any;
         toast.error(errorMessage.data.message);
       }
     }
-  }, [isSuccess, error]);
+    if (answerError) {
+      if ("data" in answerError) {
+        const errorMessage = answerError as any;
+        toast.error(errorMessage.data.message);
+      }
+    }
+  }, [isSuccess, error, answerSuccess, answerError]);
 
   const handleAnswerSubmit = () => {
-    addAnswerInQuestion({
-      questionId: questionId,
-      courseId: id,
-      contentId: data[activeVideo]._id,
-      answer,
-    });
+    if (answer.length === 0) {
+      toast.error("Answer cannot be empty");
+    } else {
+      addAnswerInQuestion({
+        answer,
+        courseId: id,
+        contentId: data[activeVideo]._id,
+        questionId,
+      });
+    }
   };
-  console.log(questionId);
 
   return (
-    <div className="w-[95%] 800px:w-[86%] py-4 m-auto ">
+    <div className="w-[95%] 800px:w-[86%] py-4 m-auto">
       <CoursePlayer
         title={data[activeVideo]?.title}
         videoUrl={data[activeVideo]?.videoUrl}
@@ -130,7 +145,7 @@ const CourseContentMedia = ({
           <AiOutlineArrowRight className="ml-2" />
         </div>
       </div>
-      <h1 className="pt-2 text-[25px] font-700 ">{data[activeVideo].title}</h1>
+      <h1 className="pt-2 text-[25px] font-[600]">{data[activeVideo].title}</h1>
       <br />
       <div className="w-full p-4 flex items-center justify-between bg-slate-500 bg-opacity-20 backdrop-blur shadow-[bg-slate-700] rounded shadow-inner">
         {["Overview", "Resources", "Q&A", "Reviews"].map((text, index) => (
@@ -146,7 +161,7 @@ const CourseContentMedia = ({
       </div>
       <br />
       {activeBar === 0 && (
-        <p className="text-[18px] whitespace-pre-line mb-3 text-black dark:text-white ">
+        <p className="text-[18px] whitespace-pre-line mb-3 text-black dark:text-white">
           {data[activeVideo]?.description}
         </p>
       )}
@@ -154,12 +169,12 @@ const CourseContentMedia = ({
         <div>
           {data[activeVideo]?.links.map((item: any, index: number) => (
             <div className="mb-5" key={index}>
-              <h2 className="800px:text-[20px] 800px:inline-block ">
+              <h2 className="800px:text-[20px] 800px:inline-block">
                 {item.title && item.title + " :"}
               </h2>
               <a
                 href={item.url}
-                className="inline-block text-[#4395c4] 800px:text-[20px] 800px:pl-2 ">
+                className="inline-block text-[#4395c4] 800px:text-[20px] 800px:pl-2">
                 {item.url}
               </a>
             </div>
@@ -188,22 +203,22 @@ const CourseContentMedia = ({
               cols={40}
               rows={5}
               placeholder="Write your queries..."
-              className="outline-none bg-transparent ml-3 border-[1px] border-[#a79898cb] 800px:w-full p-2 rounded w-[90%] 800px:text-[18px] font-Poppins "></textarea>
+              className="outline-none bg-transparent ml-3 border-[1px] border-[#a79898cb] 800px:w-full p-2 rounded w-[90%] 800px:text-[18px] font-Poppins"></textarea>
           </div>
-          <div className="w-full flex justify-end ">
+          <div className="w-full flex justify-end">
             <div
               className={`${
                 styles.button
               } !w-[120px] !h-[40px] text-[18px] mt-5 ${
                 questionCreationLoading && "cursor-not-allowed"
-              } `}
+              }`}
               onClick={questionCreationLoading ? () => {} : handleQuestion}>
               Submit
             </div>
           </div>
           <br />
           <br />
-          <div className="w-full h-[1px] bg-[#ffffff3b] "></div>
+          <div className="w-full h-[1px] bg-[#ffffff3b]"></div>
           <div>
             <CommentReply
               data={data}
@@ -235,7 +250,7 @@ const CourseContentMedia = ({
                     className="w-[50px] h-[50px] object-cover rounded-full"
                   />
                   <div className="w-full">
-                    <h5 className="pl-3 text-[20px] font-[500] dark:text-white text-black ">
+                    <h5 className="pl-3 text-[20px] font-[500] dark:text-white text-black">
                       Give a Rating <span className="text-red-500">*</span>
                     </h5>
                     <div className="flex w-full ml-2 pb-3">
@@ -267,12 +282,12 @@ const CourseContentMedia = ({
                       cols={40}
                       rows={5}
                       placeholder="Leave a Review..."
-                      className="outline-none bg-transparent ml-3 border-[1px] border-[#a79898cb] 800px:w-full p-2 rounded w-[90%] 800px:text-[18px] font-Poppins "></textarea>
+                      className="outline-none bg-transparent ml-3 border-[1px] border-[#a79898cb] 800px:w-full p-2 rounded w-[90%] 800px:text-[18px] font-Poppins"></textarea>
                   </div>
                 </div>
-                <div className="w-full flex justify-end ">
+                <div className="w-full flex justify-end">
                   <div
-                    className={`${styles.button} !w-[120px] !h-[40px] text-[18px] mt-5 800px:mr-0 mr-2 `}>
+                    className={`${styles.button} !w-[120px] !h-[40px] text-[18px] mt-5 800px:mr-0 mr-2`}>
                     Submit
                   </div>
                 </div>
@@ -295,131 +310,125 @@ const CommentReply = ({
   setQuestionId,
 }: any) => {
   return (
-    <>
-      <div className="w-full my-3">
-        {data[activeVideo].questions.map((item: any, index: any) => (
-          <CommentItem
-            data={data}
-            key={index}
-            item={item}
-            answer={answer}
-            setAnswer={setAnswer}
-            handleAnswerSubmit={handleAnswerSubmit}
-          />
-        ))}
-      </div>
-    </>
+    <div className="w-full my-3">
+      {data[activeVideo].questions.map((item: any, index: any) => (
+        <CommentItem
+          key={index}
+          item={item}
+          answer={answer}
+          setAnswer={setAnswer}
+          handleAnswerSubmit={handleAnswerSubmit}
+          setQuestionId={setQuestionId}
+          user={user}
+        />
+      ))}
+    </div>
   );
 };
 
 const CommentItem = ({
-  data,
-  key,
   item,
-  setQuestionId,
   answer,
   setAnswer,
   handleAnswerSubmit,
+  setQuestionId,
+  user,
 }: any) => {
   const [replyActive, setReplyActive] = useState(false);
 
   return (
-    <>
-      <div className="my-4">
-        <div className="flex mb-2">
-          <div>
-            <Image
-              src={
-                item.user.avatar
-                  ? item.user.avatar?.url
-                  : "../../../public/assets/avatar.png"
-              }
-              width={50}
-              height={50}
-              alt=""
-              className="w-[50px] h-[50px] object-cover rounded-full"
-            />
-          </div>
-          <div className="pl-3">
-            <h5 className="text-[20px]">{item?.user.name}</h5>
-            <p>{item?.question}</p>
-            <small className="text-[#000000b8] dark:text-[#ffffff83] ">
-              {!item.createdAt ? "10-10-2010" : format(item?.createdAt)}{" "}
-            </small>
-          </div>
-        </div>
-        <div className="w-full flex">
-          <span
-            className="800px:pl-16 text-black dark:text-[#ffffff83] cursor-pointer mr-2 "
-            onClick={() => {
-              setReplyActive(!replyActive);
-              setQuestionId(item._id);
-            }}>
-            {!replyActive
-              ? item.questionReplies.length !== 0
-                ? "All Replies"
-                : "Add Reply"
-              : "Hide Replies"}
-          </span>
-          <BiMessage
-            size={20}
-            className="cursor-pointer text-[#000000b8] dark:text-[#ffffff83]"
+    <div className="my-4">
+      <div className="flex mb-2">
+        <div>
+          <Image
+            src={
+              item.user.avatar
+                ? item.user.avatar?.url
+                : "../../../public/assets/avatar.png"
+            }
+            width={50}
+            height={50}
+            alt=""
+            className="w-[50px] h-[50px] object-cover rounded-full"
           />
-          <span className="pl-1 mt-[-4px] cursor-pointer text-[#000000b8] dark:text-[#ffffff83] ">
-            {item.questionReplies.length}
-          </span>
         </div>
-        {replyActive && (
-          <>
-            {item.questionReplies.map((item: any, index: number) => (
-              <div
-                key={index}
-                className="w-full flex 800px:ml-16 my-5 text-black dark:text-white">
-                <div>
-                  <Image
-                    src={
-                      item.user.avatar
-                        ? item.user.avatar?.url
-                        : "../../../public/assets/avatar.png"
-                    }
-                    width={50}
-                    height={50}
-                    alt=""
-                    className="w-[50px] h-[50px] object-cover rounded-full"
-                  />
-                </div>
-                <div className="pl-2">
-                  <h5 className="text-[20px]">{item.user.name}</h5>
-                  <p>{item.comment}</p>
-                  <small className="text-[#ffffff83] ">
-                    {format(item.createdAt)}
-                  </small>
-                </div>
-              </div>
-            ))}
-            <>
-              <div className="w-full flex relative">
-                <input
-                  type="text"
-                  placeholder="Enter your reply ... "
-                  value={answer}
-                  onChange={(e: any) => setAnswer(e.target.value)}
-                  className="text-black dark:text-white block 800px:ml-12 mt-2 outline-none bg-transparent border-b border-[#00000027] dark:border-[#fff] p-[5px] w-[95%]"
-                />
-                <button
-                  type="submit"
-                  className="absolute right-0 bottom-1 text-black dark:text-white  "
-                  onClick={handleAnswerSubmit}
-                  disabled={answer === ""}>
-                  Submit
-                </button>
-              </div>
-              <br />
-            </>
-          </>
-        )}
+        <div className="pl-3">
+          <h5 className="text-[20px]">{item?.user.name}</h5>
+          <p>{item?.question}</p>
+          <small className="text-[#000000b8] dark:text-[#ffffff83]">
+            {!item.createdAt ? "" : format(item?.createdAt)}
+          </small>
+        </div>
       </div>
-    </>
+      <div className="w-full flex">
+        <span
+          className="800px:pl-16 text-black dark:text-[#ffffff83] cursor-pointer mr-2"
+          onClick={() => {
+            setReplyActive(!replyActive);
+            setQuestionId(item._id);
+          }}>
+          {!replyActive
+            ? item.questionReplies.length !== 0
+              ? "All Replies"
+              : "Add Reply"
+            : "Hide Replies"}
+        </span>
+        <BiMessage
+          size={20}
+          className="cursor-pointer text-[#000000b8] dark:text-[#ffffff83]"
+        />
+        <span className="pl-1 mt-[-4px] cursor-pointer text-[#000000b8] dark:text-[#ffffff83]">
+          {item.questionReplies.length}
+        </span>
+      </div>
+      {replyActive && (
+        <>
+          {item.questionReplies.map((reply: any, index: number) => (
+            <div
+              key={index}
+              className="w-full flex 800px:ml-16 my-5 text-black dark:text-white">
+              <div>
+                <Image
+                  src={
+                    reply.user.avatar
+                      ? reply.user.avatar?.url
+                      : "../../../public/assets/avatar.png"
+                  }
+                  width={50}
+                  height={50}
+                  alt=""
+                  className="w-[50px] h-[50px] object-cover rounded-full"
+                />
+              </div>
+              <div className="pl-2">
+                <h5 className="text-[20px]">{reply.user.name}</h5>
+                <p>{reply.answer}</p>
+                <small className="text-[#ffffff83]">
+                  {format(reply.createdAt)}
+                </small>
+              </div>
+            </div>
+          ))}
+          <div className="w-full flex relative">
+            <input
+              type="text"
+              placeholder="Enter your reply..."
+              value={answer}
+              onChange={(e: any) => setAnswer(e.target.value)}
+              className="text-black dark:text-white block 800px:ml-12 mt-2 outline-none bg-transparent border-b border-[#00000027] dark:border-[#fff] p-[5px] w-[95%]"
+            />
+            <button
+              type="submit"
+              className="absolute right-0 bottom-1 text-black dark:text-white"
+              onClick={handleAnswerSubmit}
+              disabled={answer === ""}>
+              Submit
+            </button>
+          </div>
+          <br />
+        </>
+      )}
+    </div>
   );
 };
 
